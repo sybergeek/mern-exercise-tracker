035c0198e9b46d5719fc87278a08f91532ff6a58
Abhishek Sen <abhishek13dgp5@gmail.com>
2020-06-14 16:24:25 +0530

    backend CRUD operations updated

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,17 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('MongoDB database connection established successfully');
 });
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server in running on port: ${port}`);

a1fe8083aba8be45a5584f0cc18b9db945f5d762
Abhishek Sen <abhishek13dgp5@gmail.com>
2020-06-18 22:19:25 +0530

    add front UI

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -3,9 +3,12 @@ import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
 	render() {
-		return(
-			<nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+		return (
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 				<Link to="/" className="navbar-brand">ExcerTracker</Link>
+				<button className="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
 				<div className="collapse navbar-collapse">
 					<ul className="navbar-nav mr-auto">
 						<li className="navbar-item">
